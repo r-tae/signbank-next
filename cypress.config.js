e64fd43107d6734fd4027517fdf20c5555662837
River Tae Smith <22485304+r-tae@users.noreply.github.com>
2022-07-14 15:17:29 +1000

    [circleci] Pass record key to cypress
    Also removed mongo datagen config, as we won't use it anymore

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'tpbzfn',
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'cypress/results/output.xml',
